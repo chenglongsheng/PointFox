18e9fe183ca920a940e7a95b8ca95f29d906254b
cls688 <1536463948@qq.com>
2021-05-17 10:43:07 +0800

    use stylus

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="readerWrapper">
-      我是读者模块
+    <div id="title">
+      <p>我是读者模块</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name:"reader"
 
+export default {
+  name:"reader",
 }
 </script>
 
-<style>
+<style lang="stylus">
+  #readerWrapper
+    color: red;
 
 </style>
