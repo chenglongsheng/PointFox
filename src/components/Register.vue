f06c4d5ef33ecddb43b480dc3a742af1307f55e1
cls688 <1536463948@qq.com>
2021-05-24 21:39:50 +0800

    alert注册信息

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -40,7 +40,7 @@ export default {
         Axios.post("http://localhost:3000/register",{
           params:this.newUser
         }).then(function(response){
-          alert(response.data.state)
+          alert(response.data.message)
         });
       }
     }
